88605e6e2888021ea86a95e8938478a4f401e230
Lingeswaran A <125588684+Lingeswaran-A@users.noreply.github.com>
2025-01-02 23:45:07 +0530

    Time: 4 ms (98.18%), Space: 85.6 MB (87.24%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,23 +1,27 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
-        int[] res=new int[queries.length];
-        HashSet<Character> vowels = new HashSet<>(Arrays.asList('a','e','i','o','u'));
-        int[] pre_sum = new int[words.length];
-        int sum=0;
-        for (int i = 0; i < words.length; i++) {
-            String curr_word = words[i];
-            if (
-                vowels.contains(curr_word.charAt(0)) &&
-                vowels.contains(curr_word.charAt(curr_word.length() - 1))
-            ) {
-                sum++;
+        int[] count = new int[words.length+1];
+        for (int i=0;i<words.length;i++) {
+            if (check_vowel(words[i])) {
+                count[i+1]++;
             }
-            pre_sum[i] = sum;
         }
-        for (int i = 0; i < queries.length; i++) {
-            int[] curr = queries[i];
-            res[i] =pre_sum[curr[1]] - (curr[0] == 0 ? 0 : pre_sum[curr[0] - 1]);
+        for (int i=1;i<=words.length;i++) {
+            count[i]+= count[i-1];
+        }
+        int[] res = new int[queries.length];
+        int i = 0;
+        for (int[] q:queries) {
+            int l = q[0];
+            int r = q[1];
+            res[i++] = count[r+1]-count[l];
         }
         return res;
+    }
+    public boolean check_vowel(String str) {
+        return is_vowel(str.charAt(0)) && is_vowel(str.charAt(str.length()-1));
+    }
+    public boolean is_vowel(char ch) {
+        return ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u';
     }
 }
