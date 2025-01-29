2f679c30912904353aa1f68d70cdae7c67356c4c
Lingeswaran A <125588684+Lingeswaran-A@users.noreply.github.com>
2025-01-29 15:02:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -1,25 +1,21 @@
 class Solution {
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        Set<String> set=new HashSet<>(Arrays.asList(words));
-        Map<String,Boolean> memo_map=new HashMap<>();
-        List<String> res=new ArrayList<>();
-        for(String s:words){
-            if(concatable(s,set,memo_map))
-                res.add(s);
+        Set<String> s=new HashSet<>();
+        List<String> concatenateWords=new ArrayList<>();
+        for(String word:words) s.add(word);
+        for(String word:words){
+            if(checkConcatenate(word,s)) concatenateWords.add(word);
         }
-        return res;
+        return concatenateWords;
     }
-    public boolean concatable(String s,Set<String> set,Map<String,Boolean> memo_map){
-        if(memo_map.containsKey(s) && !memo_map.get(s)) return false;
-        for(int i=1;i<s.length();i++){
-            String prefix=s.substring(0,i);
-            String suffix=s.substring(i);
-            if(set.contains(prefix) && (set.contains(suffix) || concatable(suffix,set,memo_map))){
-                memo_map.put(s,true);
+    public boolean checkConcatenate(String word,Set<String> s){
+        for(int i=1;i<word.length();i++){
+            String prefixWord=word.substring(0,i);
+            String suffixWord=word.substring(i,word.length());
+            if(s.contains(prefixWord) && (s.contains(suffixWord) || checkConcatenate(suffixWord,s))){
                 return true;
             }
         }
-        memo_map.put(s,false);
         return false;
     }
 }
