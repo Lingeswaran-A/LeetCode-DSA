f56d36ed5645f9808bbcce8e15d699a5e80f77e5
Lingeswaran A <125588684+Lingeswaran-A@users.noreply.github.com>
2025-02-01 17:34:28 +0530

    Time: 1 ms (37.58%), Space: 43.1 MB (93.15%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        if(nums.length==1) return true;
+        int ind;
+        for(ind=0;ind<nums.length-1;ind++){
+            if(!isParity(nums[ind],nums[ind+1])) return false;
+            else continue; 
+        }
+        return true;
+    }
+    public boolean isParity(int a,int b){
+        if(a%2!=0 && b%2==0) return true;
+        else if(a%2==0 && b%2!=0) return true;
+        else return false;
+    }
+}
