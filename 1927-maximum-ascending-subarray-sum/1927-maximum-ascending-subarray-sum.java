a31588ce6eb202b69f23ce3da2093dc1f9f0bac8
Lingeswaran A <125588684+Lingeswaran-A@users.noreply.github.com>
2025-02-04 22:54:54 +0530

    Time: 0 ms (100%), Space: 41.6 MB (29.14%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.java
@@ -3,12 +3,7 @@ class Solution {
         int curr_sum=nums[0];
         int res=curr_sum;
         for(int i=1;i<nums.length;i++){
-            if(nums[i-1]<nums[i]){
-                curr_sum=curr_sum+nums[i];
-            }
-            else{
-                curr_sum=nums[i];
-            }
+            curr_sum=(nums[i-1]<nums[i]) ? curr_sum+nums[i] : nums[i];
             res=Math.max(res,curr_sum);
         }
         return res;
