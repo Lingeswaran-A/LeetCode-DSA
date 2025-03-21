4ef63570928074c0196cabfdcfa1d090d0ec457c
Lingeswaran A <125588684+Lingeswaran-A@users.noreply.github.com>
2025-03-21 20:17:04 +0530

    Time: 43 ms (72.63%), Space: 46.7 MB (63.13%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
@@ -1,39 +1,38 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        HashSet<String>set=new HashSet<>();
-        List<String>ans=new ArrayList<>();
-        for(String s:supplies)
-        {
-            set.add(s);
+        int n = recipes.length;
+        int[] indegree = new int[n];
+        
+        Map<String, Integer> recipeIndex = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            recipeIndex.put(recipes[i], i);
         }
-        boolean flag=true;
-        while(flag)
-        {
-            flag=false;
-                for(int i=0;i<recipes.length;i++)
-                {
-                    if(!set.contains(recipes[i]))
-                    {
-                        int x=0;
-                        for(int j=0;j<ingredients.get(i).size();j++)
-                        {
-                            if(!set.contains(ingredients.get(i).get(j)))
-                            {
-                                x=1;
-                                break;
-                            }
-                        } 
-                        if(x==0)
-                        {
-                            flag=true;
-                            set.add(recipes[i]);
-                            ans.add(recipes[i]);
-                        }
-                           
-                    }
+        
+        Map<String, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            for (String ing : ingredients.get(i)) {
+                graph.computeIfAbsent(ing, k -> new ArrayList<>()).add(i);
+                indegree[i]++;
+            }
+        }
+        
+        Queue<String> queue = new ArrayDeque<>();
+        for (String supply : supplies) {
+            queue.offer(supply);
+        }
+        
+        List<String> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            String item = queue.poll();
+            List<Integer> dependentRecipes = graph.get(item);
+            if (dependentRecipes == null) continue;
+            for (int idx : dependentRecipes) {
+                if (--indegree[idx] == 0) {
+                    result.add(recipes[idx]);
+                    queue.offer(recipes[idx]);
                 }
-
+            }
         }
-        return ans;
+        return result;
     }
 }
