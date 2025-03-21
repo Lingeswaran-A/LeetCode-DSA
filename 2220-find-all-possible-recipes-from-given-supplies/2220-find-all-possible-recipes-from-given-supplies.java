8756ade78e13ca569f081e26eeff001d54c7d888
Lingeswaran A <125588684+Lingeswaran-A@users.noreply.github.com>
2025-03-21 20:17:23 +0530

    Time: 40 ms (77.37%), Space: 46.9 MB (52.98%) - LeetHub

--- 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
+++ 2220-find-all-possible-recipes-from-given-supplies/2220-find-all-possible-recipes-from-given-supplies.java
@@ -2,12 +2,10 @@ class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
         int n = recipes.length;
         int[] indegree = new int[n];
-        
         Map<String, Integer> recipeIndex = new HashMap<>();
         for (int i = 0; i < n; i++) {
             recipeIndex.put(recipes[i], i);
         }
-        
         Map<String, List<Integer>> graph = new HashMap<>();
         for (int i = 0; i < n; i++) {
             for (String ing : ingredients.get(i)) {
@@ -15,12 +13,10 @@ class Solution {
                 indegree[i]++;
             }
         }
-        
         Queue<String> queue = new ArrayDeque<>();
         for (String supply : supplies) {
             queue.offer(supply);
         }
-        
         List<String> result = new ArrayList<>();
         while (!queue.isEmpty()) {
             String item = queue.poll();
