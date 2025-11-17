79c67c1c864ec2656f68db6675e98a1bc8e6f2ff
Lingeswaran A <125588684+Lingeswaran-A@users.noreply.github.com>
2025-11-17 21:26:05 +0530

    Time: 1 ms (99.71%), Space: 65.6 MB (5.71%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.java
@@ -1,15 +1,14 @@
 class Solution {
     public boolean kLengthApart(int[] nums, int k) {
-        boolean isValid=true;
         int index=-1;
         for(int i=0;i<nums.length;i++){
             if(nums[i]==1){
                 if(index!=-1 && i-index-1<k){
-                    isValid=false;
+                    return false;
                 }
                 index=i;
             }
         }
-        return isValid;
+        return true;
     }
 }
